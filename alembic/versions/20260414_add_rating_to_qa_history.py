6e748ba82efc74ad74b49a090396100f1c3fd246
edward1503 <nguyendonduc1503@gmail.com>
2026-04-14 14:33:09 +0700

    Create 20260414_add_rating_to_qa_history.py

---
+++ alembic/versions/20260414_add_rating_to_qa_history.py
@@ -0,0 +1,23 @@
+"""add rating to qa_history
+
+Revision ID: 20260414_add_rating
+Revises: e56e139d2676
+Create Date: 2026-04-14 07:30:00.000000
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+# revision identifiers, used by Alembic.
+revision = '20260414_add_rating'
+down_revision = 'e56e139d2676'
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    op.add_column('qa_history', sa.Column('rating', sa.Integer(), nullable=True))
+
+
+def downgrade() -> None:
+    op.drop_column('qa_history', 'rating')
